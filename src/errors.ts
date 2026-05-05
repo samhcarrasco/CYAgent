f9b1ba99d093898e8710ec4d3acce9210b2e3091
samhcarrasco <samhcarrasco@gmail.com>
2026-05-04 17:34:34 -0700

    core: add error and atomic write helpers

---
+++ src/errors.ts
@@ -0,0 +1,9 @@
+﻿export class CyaError extends Error {
+  constructor(
+    public readonly code: string,
+    message: string,
+  ) {
+    super(message);
+    this.name = 'CyaError';
+  }
+}
---
+++ src/io.ts
@@ -0,0 +1,7 @@
+﻿import { writeFile, rename } from 'node:fs/promises';
+
+export async function atomicWrite(filePath: string, content: string): Promise<void> {
+  const tmp = `${filePath}.tmp`;
+  await writeFile(tmp, content, 'utf8');
+  await rename(tmp, filePath);
+}
